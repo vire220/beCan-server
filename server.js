df5170daf74a9846a616565dd618c392004db016
Alex McCreadie <vire220@yahoo.ca>
2018-03-01 02:33:59 +0000

    Added logging

--- server.js
+++ server.js
@@ -1,27 +1,44 @@
 var express = require('express'),
   app = express(),
+  winston = require('winston'),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Step = require('./api/models/becanServerModel').step, //created model loading here
   bodyParser = require('body-parser');
+  
+  
+winston.level = 'debug';
+winston.add(winston.transports.File, {filename: 'server.log', json: false});
 
-// mongoose instance connection url connection
+winston.info("Starting Log");
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/becandb');
-
+mongoose.connect('mongodb://localhost/becandb', null, function(err){
+  if(err){
+    winston.error(err);
+    process.exit(1);
+  } else {
+    winston.info("Connected to MongoDB");
+  }
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/becanServerRoutes'); //importing route
-routes(app); //register the route
+app.use(function(req, res, next) {
+   winston.info("Request to " + req.path);
+   next();
+});
 
+var routes = require('./api/routes/becanServerRoutes');
+routes(app);
 
 app.use(function(req, res) {
+  winston.info("Invalid request");
   res.status(404).send({ url: req.originalUrl + ' not found' })
 });
 
 app.listen(port);
 
 
-console.log('beCan RESTful API server started on: ' + port);
+winston.info("Server started, listening on: " + port);
